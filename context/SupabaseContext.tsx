960fe0d866a39eb69fa1e2a12a1ea517b9a70fa8
Andie Similon <andie.similon@ap.be>
2025-11-24 14:07:36 +0100

    More stuff

--- context/SupabaseContext.tsx
+++ context/SupabaseContext.tsx
@@ -1,16 +1,31 @@
-import { createContext, ReactNode, useContext } from "react";
+import { Session } from "@supabase/supabase-js";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 interface SupabaseContextProps {
-
+    session: Session | null;
+    login: (email: string, password: string) => Promise<void>;
+    logout: () => Promise<void>;
+    loggingIn: boolean;
+    initializing: boolean;
 }
 
-const SupabaseContext = createContext<SupabaseContextProps>({});
+const SupabaseContext = createContext<SupabaseContextProps | undefined>(undefined);
 
 const SupabaseProvider = ({children} : { children: ReactNode}) => {
+    const [loggingIn, setLoggingIn] = useState<boolean>(false);
+    const [initializing, setInitializing] = useState<boolean>(true);
+    const [session, setSession] = useState<Session | null>(null);
 
+    const login = async() => {
+
+    }
+
+    const logout = async() => {
+
+    }
 
     return (
-        <SupabaseContext.Provider value={{}}>
+        <SupabaseContext.Provider value={{loggingIn, initializing, session, login, logout}}>
 
         </SupabaseContext.Provider>
     )
