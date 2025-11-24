2a3adb777760058e8365c49f25f32a03152e9362
Andie Similon <andie.similon@ap.be>
2025-11-24 15:35:02 +0100

    supabase shit added

--- app/dashboard/index.tsx
+++ app/dashboard/index.tsx
@@ -2,13 +2,21 @@ import { View } from "react-native";
 import { Text } from "@/components/ui/text";
 import { Button } from "@/components/ui/button";
 import { useSupabase } from "@/context/SupabaseContext";
+import MapView, { Marker } from "react-native-maps";
 
 const MapScreen = () => {
     const { logout } = useSupabase();
 
     return (
-        <View>
+        <View className="flex-1">
             <Button onPress={() => logout()}><Text>Logout</Text></Button>
+
+            <MapView style={{flex: 1}}>
+                <Marker coordinate={{
+                    latitude: 50.11,
+                    longitude: 2
+                }} title="Ergens in frankrijk"/>
+            </MapView>
         </View>
     )
 }
