fa2dfe38daf4f4d91f010d5ced9c6a1df1ddba6d
Andie Similon <andie.similon@ap.be>
2025-11-24 13:54:31 +0100

    Initial commit

---
+++ lib/supabase.ts
@@ -0,0 +1,27 @@
+import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AppState } from "react-native";
+
+const supabaseURL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseURL || !supabasePublishableKey) {
+    throw new Error("Missing supabase environment variables");
+}
+
+export const supabase = createClient(supabaseURL, supabasePublishableKey, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false
+    }
+});
+
+AppState.addEventListener("change", (state) => {
+    if (state === "active") {
+        supabase.auth.startAutoRefresh();
+    } else {
+        supabase.auth.stopAutoRefresh();
+    }
+})
